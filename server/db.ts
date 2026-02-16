071bb1af9ac99dae9fb710255acdba8a5f73bf54
tewascript <ogunodemarvellous@gmail.com>
2026-02-16 14:02:38 +0100

    fixed issue with env

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,12 @@
 import { drizzle } from "drizzle-orm/neon-serverless";
 import { Pool } from "@neondatabase/serverless";
 import * as schema from "@shared/schema";
+import dotenv from "dotenv";
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?"
+    "DATABASE_URL must be set"
   );
 }
 
